5d69e83ad30fc8b90eac4df79e761f003f7c7e64
elavalasrinivasreddy <elavalasrinivasreddy@gmail.com>
2026-01-29 14:40:53 +0530

    ai news summarizer node and graph updated with methods.

--- src/workflow/graphs/ai_news_summarizer_graph.py
+++ src/workflow/graphs/ai_news_summarizer_graph.py
@@ -1,11 +1,11 @@
 from langgraph.graph import StateGraph, END
-
-
+from src.workflow.state import ChatbotState
+from src.workflow.nodes.ai_news_summarizer_node import AINewsSummarizerNode
 
 class AINewsSummarizerGraph:
     def __init__(self, model):
         self.model = model
-        self.graph = StateGraph(AINewsSummarizerState)
+        self.graph = StateGraph()
         
     def build_graph(self):
         """
@@ -13,13 +13,14 @@ class AINewsSummarizerGraph:
         """
 
         # add nodes
-        self.graph.add_node("fetch_ai_news", "")
-        self.graph.add_node("summarize_ai_news", "")
-        self.graph.add_node("save_ai_results", "")
+        ai_news_summarizer_node = AINewsSummarizerNode(self.model)
+        self.graph.add_node("fetch_ai_news", ai_news_summarizer_node.fetch_ai_news)
+        self.graph.add_node("summarize_ai_news", ai_news_summarizer_node.summarize_ai_news)
+        self.graph.add_node("save_ai_results", ai_news_summarizer_node.save_ai_results)
 
         # add edges
         self.graph.set_entry_point("fetch_ai_news")
-        self.graph.add_edge("fetch_ai_news", "summarize_ai_ news")
+        self.graph.add_edge("fetch_ai_news", "summarize_ai_news")
         self.graph.add_edge("summarize_ai_news", "save_ai_results")
         self.graph.add_edge("save_ai_results", END)
         
--- src/workflow/nodes/ai_news_summarizer_node.py
+++ src/workflow/nodes/ai_news_summarizer_node.py
@@ -1,4 +1,5 @@
-    from tavily import TavilyClient
+from tavily import TavilyClient
+from langchain_community.prompts import ChatPromptTemplate
 
 class AINewsSummarizerNode:
     def __init__(self, model):
@@ -29,3 +30,54 @@ class AINewsSummarizerNode:
         state["news_data"] = response.get('results', [])
         self.state["news_data"] = state["news_data"]
         return state
+
+    def summarize_ai_news(self, state:dict) -> dict:
+        """
+        This function summarizes the latest AI news.
+        """
+
+        system_prompt = """
+        You are an expert AI news summarizer. Your task is to summarize the latest AI news from the web and provide in markdown format.
+        For each item include:
+        - Date in dd-mm-yyyy format
+        - Title in bold
+        - Concise summary of the news
+        - URL in markdown format
+
+        Output Format:
+        #### [Date] 
+        **Title**
+        [Summary (URL)]
+        """
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", system_prompt),
+            ("user", "Summarize the below latest AI news from the web: \n{news_data}")
+        ])
+
+        # Create new content
+        news_data = state["news_data"]
+        new_content = "\n\n".join(
+            f"content: {news['content']}\nurl: {news['url']}\ndate: {news['published_date']}\ntitle: {news['title']}" for news in news_data
+        )
+
+        # Invoke the model
+        response = self.model.invoke(prompt.format(news_data=new_content))
+        state["summary"] = response.content
+        self.state["summary"] = state["summary"]
+        return self.state
+
+    def save_ai_results(self, state:dict) -> dict:
+        """
+        This function saves the AI news results.
+        """
+        # Save the results to a file
+        frequency = self.state["frequency"]
+        summary = self.state["summary"]
+        file_name = f"./data/ai_news_results_{frequency}.md"
+        self.state["file_name"] = file_name 
+        with open(file_name, "w") as f:
+            f.write(f"# AI News Results ({frequency.capitalize()})\n\n")
+            f.write(summary)
+        return self.state
+
+        
